ee58801ef55b85fad1ba1d770336d1acec10478e
Gil Silva <gil.marques11@gmail.com>
2026-04-27 23:28:18 +0200

    Update fetch-uipath.js

--- functions/api/fetch-uipath.js
+++ functions/api/fetch-uipath.js
@@ -106,7 +106,7 @@ export async function onRequestPost({ request }) {
       client_id: clientId, 
       client_secret: clientSecret,
       // Use specific scopes instead of OR.Default
-      scope: 'OR.Folders.Read OR.Execution.Read OR.Machines.Read'
+      scope: 'OR.Folders.Read OR.Execution.Read OR.Machines.Read OR.Users.Read OR.Queues.Read OR.Assets.Read'    
     });
 
     let tokenRes;
